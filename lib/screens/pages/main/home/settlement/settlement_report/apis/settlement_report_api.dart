4fbbabbfcdf30b07698d186137a1e3da7a144d15
rapeapach <kagaminelenkyun@gmail.com>
2021-12-19 01:47:37 +0700

    Add delay and TODO note to mock API

--- lib/screens/pages/main/home/settlement/settlement_report/apis/settlement_report_api.dart
+++ lib/screens/pages/main/home/settlement/settlement_report/apis/settlement_report_api.dart
@@ -40,6 +40,9 @@ class SettlementReportApi {
     required DateTime date,
     required String accessToken,
   }) async {
+    // TODO: use real data
+    await Future.delayed(Duration(seconds: 1));
+
     final now = DateTime.now();
     final date = DateTime(now.year, now.month, now.day);
 
